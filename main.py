35b59cd6f84f776bb7ab1b89f7beadd36c00e9c7
Rory Owens <40439757@live.napier.ac.uk>
2023-10-08 22:34:29 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+
+
+
+class loadbalancer:
+    def initlb(self):
+        # Intialise load balancer
+        self.pool = []
+    
+    def addServer():
+        # Add server to a pool
+        pass
+    
+    def shutdownServer():
+        # This will shut down a server from a pool
+        pass
+    
+    def assignRequest():
+        # This will contain load balancing algorithm
+        pass
+    
+    
+    
+
+class server:
+    def initserv(self):
+        # Code for the server class goes here
+        pass
+    
+    
+class Client:
+    def initclient(self):
+        pass
+    
+    def makeRequest():
+        # Generates request to the load balancer
+        pass
+    
+    
