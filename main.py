5a9cd6afec43c950f4742d7a45c1db37e06fa81f
Rory Owens <40439757@live.napier.ac.uk>
2023-10-09 16:55:20 +0100

    Added Source IP to packet using client IP

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import queue
 
 
 noOfServers = random.randint(1,5)
+noOfRequests = 5
 
 class Packet:
-    def __init__(self, content):
+    def __init__(self, content, ip):
         self.content = content
+        self.source_ip = ip
 
 class LoadBalancer:
     def __init__(self):
@@ -27,14 +29,15 @@ class LoadBalancer:
         packet = self.request_queue.get()
 
         selected_server = random.choice(self.pool)
-        return f"Request {packet.content} handled by {selected_server}"
+        return f"Request {packet.content} from {packet.source_ip} handled by {selected_server}"
 
 class Client:
     def __init__(self, load_balancer):
         self.load_balancer = load_balancer
+        self.ip_add = f"10.10.0.{random.randint(1,254)}"
 
-    def make_request(self, content):
-        packet = Packet(content)
+    def make_request(self, content, source_ip):
+        packet = Packet(content, source_ip)
         self.load_balancer.request_queue.put(packet)
 
 if __name__ == "__main__":
@@ -54,9 +57,13 @@ if __name__ == "__main__":
     client2 = Client(lb)
 
     # Sequential requests
-    for i in range(5):
-        client1.make_request(f"Request{i}")
+    for i in range(noOfRequests):
+        client1.make_request(f"Request{i+1}", client1.ip_add)
+        
+     # Sequential requests
+    for i in range(noOfRequests):
+        client2.make_request(f"Request{i+1}", client2.ip_add)
 
-    for _ in range(5):
+    for _ in range(noOfRequests*2):
         result = lb.distribute_request()
         print(result)
